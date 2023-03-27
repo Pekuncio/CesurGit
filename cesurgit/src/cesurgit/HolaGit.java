9b2fad4d3ea5eccb5f6b295b8388b208ca16c38b
pekuncio <xiscotenor94hotmail.es>
2023-03-27 11:49:56 +0200

    Sumar numeros

--- cesurgit/src/cesurgit/HolaGit.java
+++ cesurgit/src/cesurgit/HolaGit.java
@@ -4,6 +4,13 @@ public class HolaGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
+		
+		System.out.println("Sumar en java");
+		
+		int a=1;
+		int b =3;
+		
+		System.out.println("suma 2 numeros = " + (a+b));
 	}
 
 }
