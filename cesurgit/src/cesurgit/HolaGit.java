8becb658328871b2711d4c534a96dc3c37b41943
pekuncio <xiscotenor94hotmail.es>
2023-03-27 11:48:55 +0200

    Primera Modificación.

---
+++ cesurgit/src/cesurgit/HolaGit.java
@@ -0,0 +1,9 @@
+package cesurgit;
+
+public class HolaGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Git");
+	}
+
+}
---
+++ cesurgit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PekuPC
+ *
+ */
+module cesurgit {
+}
